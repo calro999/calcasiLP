66c06b4e3fc6bc71be9121a470c3f14e2e7f67ae
calro999 <134254994+calro999@users.noreply.github.com>
2025-05-23 10:41:35 +0000

    site

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 // next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://calcasi-lp.vercel.app/',
-  generateRobotsTxt: true, // ← robots.txt も作る
-  generateIndexSitemap: false, // インデックス用sitemapは不要ならfalse
+  siteUrl: 'https://calcasi-lp.vercel.app', // ← 必ずこれ！
+  generateRobotsTxt: true,
+  generateIndexSitemap: false
 };
