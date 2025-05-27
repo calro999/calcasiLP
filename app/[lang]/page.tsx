827de15f9dfd8c82e7b2a497ad98f4e41cf27b43
calro999 <gamingmedia.mail@gmail.com>
2025-05-27 21:45:14 +0700

    page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -6,13 +6,19 @@ import Features from "@/components/features";
 import CasinoDetails from "@/components/casino-details";
 import PopularGames from "@/components/popular-games";
 import CTA from "@/components/cta";
+import { getAllArticles } from "@/lib/getAllArticles";
 
 export const metadata: Metadata = {
   title: "カジノ比較ならCalcasi！",
-  description: "Calcasiは、人気オンラインカジノのランキング・ゲーム紹介・初心者ガイドを提供するカジノ比較サイトです。",
+  description: "人気オンラインカジノのランキング・ゲーム紹介・初心者ガイドを提供するカジノ比較サイトです。",
 };
 
-export default function LangHome() {
+export default async function LangHome({
+  params,
+}: {
+  params: { lang: "ja" | "en" };
+}) {
+  const articles = await getAllArticles(params.lang);
   return (
     <main>
       <Hero />
