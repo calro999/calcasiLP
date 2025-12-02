c5454c142bb186d320e646c7dc1572af0a2f56a1
calro999 <gamingmedia.mail@gmail.com>
2025-12-02 13:40:50 +0700

    14

---
+++ contents/articles/ja/meta.ts
@@ -0,0 +1,30 @@
+import Article13 from "./13.mdx";
+import Article14 from "./14.mdx";
+
+interface ArticleMapItem {
+  id: number;
+  title: string;
+  date: string;
+  readTime: string;
+  category: string;
+  author: string;
+  image: string;
+  component: any;
+}
+
+interface ArticleMap {
+  [key: string]: ArticleMapItem;
+}
+
+export const articles: ArticleMap = {
+  "14": {
+    id: 14,
+    title: "仮記事MDXテスト",
+    date: "2025-12-02",
+    readTime: "5分",
+    category: "テスト",
+    author: "テスト太郎",
+    image: "/placeholder.jpg",
+    component: Article14,
+  },
+};
